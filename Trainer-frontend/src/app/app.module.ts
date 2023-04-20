90903d660256eedd879a8cdc533fd608671b4359
Pati <patrickmistlberger@gmail.com>
2023-04-20 21:58:14 +0200

    init frontend

--- Trainer-frontend/src/app/app.module.ts
+++ Trainer-frontend/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ShellComponent } from './components/shell/shell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShellComponent
   ],
   imports: [
     BrowserModule,
